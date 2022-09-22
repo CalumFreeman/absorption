75587cc3f0ec410c853936574a654f122f4dddd3
Calum Freeman <calum.freeman@gmail.com>
2022-09-22 14:07:54 +0100

    Build geometry/materials on the fly as slices

--- src/GREENERDetectorConstruction.cc
+++ src/GREENERDetectorConstruction.cc
@@ -70,8 +70,6 @@ void GREENERDetectorConstruction::DefineMaterials()
   //  Material Information imported from NIST database. 
   G4NistManager* NISTman = G4NistManager::Instance();
 
-  capsuleMaterial   = NISTman->FindOrBuildMaterial("G4_WATER");
-
   static constexpr double universe_mean_density = 1.e-25*g/cm3;
   Vacuum1 = new G4Material("VACUUM1",
                            1.,
@@ -79,10 +77,76 @@ void GREENERDetectorConstruction::DefineMaterials()
                            universe_mean_density);
 }
 
+G4Material* get_mat_at_rho(G4double density){
+  // define variables
+  G4String name, symbol;
+  G4double a, z;
+  G4int iz, n, nel, natoms, ncomponents;
+
+  // define Elements
+  a = 1.01*g/mole;
+  G4Element* elH = new G4Element(name="Hydrogen", symbol="H" , z= 1., a);
+  a = 16.00*g/mole;
+  G4Element* elO = new G4Element(name="Oxygen", symbol="O" , z= 8., a);
+  a = 12.01*g/mole;
+  G4Element* elC = new G4Element(name="Carbon", symbol="C" , z= 6., a);
+
+  G4Material* H2O = new G4Material(name="Water", density, ncomponents=2);
+  H2O->AddElement(elH, natoms=2);
+  H2O->AddElement(elO, natoms=1);
+
+  G4Material* CH = new G4Material(name="CH", density, ncomponents=2);
+  CH->AddElement(elH, natoms=1);
+  CH->AddElement(elC, natoms=1);
+
+  return CH;
+}
+
+void make_step(int i, int n, G4LogicalVolume* worldLogical){
+  // Construct a box with the given parameters
+  G4Box* capsuleSolid;
+  G4LogicalVolume* capsuleLogical;
+  G4Material* material;
+
+  G4double density = (0.4+2*i/10.0)*g/cm3;
+  material = get_mat_at_rho(density);
+
+  G4double halfx = 0.001;
+  capsuleSolid = new G4Box("Capsule", //its name
+                            halfx*m/n, // x half side length
+                            0.0005*m, // y half side length
+                            0.0025*m); // z half side length
+                                      // z is the axis of the source -> detector
+
+  capsuleLogical = new G4LogicalVolume(capsuleSolid,       //its solid
+                                        material,    //its material
+                                        "CapsuleLV");          //its name
+
+  // rotation
+  G4RotationMatrix* capsuleRotMat =  new G4RotationMatrix();
+  capsuleRotMat->rotateZ(0);
+
+  // calculate the position of this block
+  G4ThreeVector boxPosition (-m*(n-2*i-1)*halfx/n,
+                             0,
+                             0);
+
+  new G4PVPlacement(capsuleRotMat,              //its rotation
+                    boxPosition,       //its translation
+                    capsuleLogical,      //its logical volume
+                    "Capsule",              //its name
+                    worldLogical,       //its mother volume
+                    false,               //no boolean operation
+                    0,                   //copy number
+                    true);     //checking overlaps
+
+
+}
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 void GREENERDetectorConstruction::SetupGeometry()
 {
-  //     
+  //
   // World
   //
   G4VSolid* worldSolid = new G4Box("World",6.*m,6.*m,6.*m);
@@ -97,39 +161,10 @@ void GREENERDetectorConstruction::SetupGeometry()
                                     0,
                                     false,
                                     0);
-
-  // Construct a box with the given parameters
-  G4Box* capsuleSolid;
-  G4LogicalVolume* capsuleLogical;
-
-  capsuleSolid = new G4Box("Capsule", //its name
-                            0.0005*m, // x half side length
-                            0.0005*m, // y half side length
-                            0.0025*m); // z half side length
-                                      // z is the axis of the source -> detector
-
-  capsuleLogical = new G4LogicalVolume(capsuleSolid,       //its solid
-                                        capsuleMaterial,    //its material
-                                        "CapsuleLV");          //its name
-
-  // rotation
-  G4RotationMatrix* capsuleRotMat =  new G4RotationMatrix();
-  capsuleRotMat->rotateZ(0);
-
-  // calculate the position of this block
-  G4ThreeVector boxPosition (0,
-                             0,
-                             0);
-
-  new G4PVPlacement(capsuleRotMat,              //its rotation
-                    boxPosition,       //its translation
-                    capsuleLogical,      //its logical volume
-                    "Capsule",              //its name
-                    worldLogical,       //its mother volume
-                    false,               //no boolean operation
-                    0,                   //copy number
-                    true);     //checking overlaps
-
+  int n = 15;
+  for (int i = 0; i<n; i++) {
+    make_step(i, n, worldLogical);
+  }
 
   G4cout << "GREENER: Geometry done" << std::endl;
 
